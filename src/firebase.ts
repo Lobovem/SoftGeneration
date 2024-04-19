95231b3060598b3d6aa0e6c9f9ea091b0d43a525
Lobov.EM <lobov.em@gmail.com>
2024-04-19 18:53:22 +0300

    feat(Firebase): add firebase plugin to project

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const API_KEY = import.meta.env.VITE_API_KEY
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: "softgeneration-a0e40.firebaseapp.com",
+  projectId: "softgeneration-a0e40",
+  storageBucket: "softgeneration-a0e40.appspot.com",
+  messagingSenderId: "856851778746",
+  appId: "1:856851778746:web:4a4a2ce1ee2a27779f8683"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
